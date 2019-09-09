3169ce0dcd22d106612c0c1796b8f66804e3e6b2
Adnan <adnan.chowdhury@outlook.com>
2019-09-09 16:19:18 -0500

    Update endpoint in PUT

--- server.js
+++ server.js
@@ -37,7 +37,17 @@ server.post("/", ValidateResource, (req, res) => {
     .catch(err => res.json(err));
 });
 
-server.put("/:id", (req, res) => {});
+server.put("/:id", (req, res) => {
+  const changes = req.body;
+
+  db("accounts")
+    .where({ id: req.params.id })
+    .update(changes)
+    .then(count => {
+      res.status(200).json({ RecordUpdated: count });
+    })
+    .catch(err => res.json(err));
+});
 
 server.delete("/:id", (req, res) => {});
 
