6fd66e76b9000a45c9a18ef5dde75fd656957ed6
Adnan <adnan.chowdhury@outlook.com>
2019-09-09 15:46:15 -0500

    GET endpoint and db access

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  db.select("*")
+    .from("accounts")
+    .then(accounts => res.status(200).json(accounts))
+    .catch(error => res.status(500).json(error));
+});
+
+server.post("/", (req, res) => {});
+
+server.put("/:id", (req, res) => {});
+
+server.delete("/:id", (req, res) => {});
 
 module.exports = server;
