ff8d5d509572c1c4ff5c5cdc5425ae0c988a98a2
Adnan <adnan.chowdhury@outlook.com>
2019-09-09 16:02:31 -0500

    Get by ID endpoiint, custom middleware for validation

--- server.js
+++ server.js
@@ -13,10 +13,34 @@ server.get("/", (req, res) => {
     .catch(error => res.status(500).json(error));
 });
 
-server.post("/", (req, res) => {});
+server.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("accounts")
+    .where({ id })
+    .first()
+    .then(posts => res.status(200).json(posts))
+    .catch(err => res.json(err));
+});
+
+server.post("/", ValidateResource, (req, res) => {});
 
 server.put("/:id", (req, res) => {});
 
 server.delete("/:id", (req, res) => {});
+
+function ValidateResource(req, res, next) {
+  if (req.body.name === undefined) {
+    res.status(400).json({
+      errorMessage: "Make sure your project has name field"
+    });
+  } else if (req.body.budget === undefined) {
+    res.status(400).json({
+      errorMessage: "Make sure your project has budget field"
+    });
+  } else {
+    next();
+  }
+}
 
 module.exports = server;
